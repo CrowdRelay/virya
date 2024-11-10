a3a705b4fc72f1d5611e0ada515f898dedba425d
Wojciech Bator <wojciech.bator@g2a.com>
2024-11-10 19:13:35 +0100

    links

--- src/components/shows/show.js
+++ src/components/shows/show.js
@@ -8,12 +8,15 @@ const ShowItem = ({ item }) => {
         let isToday = d.setHours(0,0,0,0) === today.setHours(0,0,0,0);
         let textDate = isToday ? 'Today - ' : 'Date - ';
         isToday ? baseStyles += " bg-red-900" : baseStyles += " bg-black";
-        return <div className={baseStyles}>
-            <h2 className="lg:text-2xl text-xs lg:ml-2">{item.title}</h2>
+        return <div onClick={event => {
+            event.preventDefault();
+            item.event && window.open(item.event, '_blank').focus();
+        }} className={baseStyles}>
+            <h2 className="lg:text-2xl text-md lg:ml-2">{item.title}</h2>
             <div className='lg:flex lg:flex-row lg:flex-grow place-items-center justify-end'>
-                <h2 className="lg:text-2xl text-xs"><p className='inline'>{textDate}</p> {d.toLocaleDateString("pl-PL")}</h2>
-                {item.event && <a title="Event" href={item.event} rel="noreferrer" target="_blank"><button className="lg:px-6 lg:py-4 p-2 mx-6 my-8 btn-outline-primary transition duration-500 ease-in-out focus:outline-none focus:shadow-outline border border-amber-300 hover:bg-amber-200 text-amber-300 hover:text-black font-normal px-4 rounded">Event</button></a>}
-                {item.tickets && <a title="Tickets" href={item.tickets} rel="noreferrer" target="_blank"><button className="lg:px-6 lg:py-4 p-2 mx-6 my-8 btn-outline-primary transition duration-500 ease-in-out focus:outline-none focus:shadow-outline border border-amber-300 hover:bg-amber-200 text-amber-300 hover:text-black font-normal px-4 rounded">Tickets</button></a>}
+                <h2 className="lg:text-2xl lg:my-8 text-md"><p className='inline'>{textDate}</p> {d.toLocaleDateString("pl-PL")}</h2>
+                {item.event && <a title="Event" href={item.event} rel="noreferrer" target="_blank"><button className="lg:px-6 lg:py-4 p-2 mx-6 lg:my-8 my-4 btn-outline-primary transition duration-500 ease-in-out focus:outline-none focus:shadow-outline border border-amber-300 hover:bg-amber-200 text-amber-300 hover:text-black font-normal px-4 rounded">Event</button></a>}
+                {item.tickets && <a title="Tickets" href={item.tickets} rel="noreferrer" target="_blank"><button className="lg:px-6 lg:py-4 p-2 mx-6 lg:my-8 my-4 btn-outline-primary transition duration-500 ease-in-out focus:outline-none focus:shadow-outline border border-amber-300 hover:bg-amber-200 text-amber-300 hover:text-black font-normal px-4 rounded">Tickets</button></a>}
             </div>
         </div>
     }
