0183e78b93bbef2fc54c3746ecf230f0c6420733
Wojciech Bator <wojciech.bator@g2a.com>
2025-01-07 12:53:10 +0100

    gigi

--- src/components/shows/shows.js
+++ src/components/shows/shows.js
@@ -14,7 +14,7 @@ const Shows = memo(() => {
         <div className="bg-stone-900 py-8 lg:px-8 lg:rounded-b-2xl">
             <div className="text-center mb-8 mx-4">
                 <p className="text-4xl font-bold leading-tight my-4">Shows</p>
-                <p className="italic w-full my-4">Make sure you're not skipping our upcoming shows</p>
+                <p className="italic w-full my-4">Make sure you're not missing out our upcoming shows</p>
                 <div className="grid grid-cols-1 gap-4 overflow-hidden relative my-8">
                     {showItems}
                 </div>
