21808459396ac3b22ca474abf9592dc87bf5fb2b
Wojciech Bator <wojciech.bator@g2a.com>
2026-06-22 11:28:47 +0200

    dark color scheme

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -9,8 +9,11 @@ export const wrapPageElement = ({ element, props }) => (
 
 const LANG_REDIRECT = `(function(){try{var p=location.pathname;if(p==='/pl'||p.indexOf('/pl/')===0)return;var pref=localStorage.getItem('virya-lang');if(pref!=='pl'&&pref!=='en'){pref=(navigator.language||'').toLowerCase().indexOf('pl')===0?'pl':'en';}if(pref==='pl'){location.replace('/pl'+p+location.search+location.hash);}}catch(e){}})();`
 
-export const onRenderBody = ({ setHtmlAttributes, setPreBodyComponents }) => {
+export const onRenderBody = ({ setHtmlAttributes, setHeadComponents, setPreBodyComponents }) => {
   setHtmlAttributes({ lang: `en` })
+  setHeadComponents([
+    <meta key="color-scheme" name="color-scheme" content="dark" />,
+  ])
   setPreBodyComponents([
     <script
       key="virya-lang-redirect"
